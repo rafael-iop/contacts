5500f2fc03e203a373e9cb399eb538e7f021d171
Rafael Iop <rafael@wp8.com.br>
2018-06-01 01:25:36 -0300

    Contacts migration and model.

---
+++ app/Models/Contact.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Contact extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 
+        'last_name', 
+        'email', 
+        'phone',
+    ];
+
+}
